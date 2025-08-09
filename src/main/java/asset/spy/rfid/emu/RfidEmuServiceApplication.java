9fe9405d84c75734bff083ad35d3e94e5f9c57f5
Nastss6tx5 <Nastasiia227@gmail.com>
2025-08-09 18:16:34 +0300

    feat(STOCK-109) fixed portType, fixed description openAPI, springboot version

--- src/main/java/asset/spy/rfid/emu/RfidEmuServiceApplication.java
+++ src/main/java/asset/spy/rfid/emu/RfidEmuServiceApplication.java
@@ -1,11 +1,15 @@
 package asset.spy.rfid.emu;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@OpenAPIDefinition(info = @Info(title = "Asset spy rfid emulator service API",
+        description = "Rest API for rfid emulator service", version = "1.0.0"))
 public class RfidEmuServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RfidEmuServiceApplication.class, args);
